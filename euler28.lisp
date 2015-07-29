71fb49cff343bf8ce25c96da7f31ee14c36b6ad2
Andreas Davour <AndreasDavour@users.noreply.github.com>
2015-07-29 14:00:16 +0200

    28: got the mainloop to work. Not pretty, but works.

--- euler28.lisp
+++ euler28.lisp
@@ -15,40 +15,6 @@
 ;;; What is the sum of the numbers on the diagonals in a 1001 by 1001
 ;;; spiral formed in the same way?
 
-(defparameter apa (make-array '(1001 1001)))
-
-(defparameter matrix-size (* 1001 1001))
-
-;; set the last entry on first row
-(setf (aref apa 0 1000) matrix-size)
-
-;; row-major-aref
-
-(loop for i from 1 to 4
-      do )
-
-find x evens
-
-;; (13 14 15 16 17 18 19 20 21 22 23 24 25)
-
-;; In order to get the four numbers from the list above
-;; sort out the number of even numbers.
-;; In this iteration it is 2, and returns
-;; (13 17 21 25)
-;; Then skipt ahead a set amount to find the next range
-;; which will be (31 37 43 49)
-
-;; this attempt to just get four numbers recurse forever
-;; time for a refactor branch
-
-(defun euler28 (interval)
-(loop until number = (* 1001 1001)
-      interval = interval + 1
- (loop 
-   get next odd number with an interval of N
-   until you have four)))
-
-;; oddies x 2:: skip -> x:: oddies x+1 3:: skip -> x:: oddies x+1 4
 
 (defun produce-four-numbers (start intermediates)
 "This will return the next four numbers to add up."
@@ -72,3 +38,14 @@ find x evens
     (if (evenp c)
 	(incf evens))))
 
+;;; main loop
+  
+(defun euler28 (max)
+  (apply #'+ 
+	 (do* ((n 2 (incf n))
+	       (temp (produce-four-numbers 2 1))
+	       (total (cons 1 temp)
+		      (setf total (append total temp))))
+	      ((= (* max max) (fourth temp)) total)
+	   (setf next (skip-to-next-range (fourth temp) n))
+	   (setf temp (produce-four-numbers next n)))))
